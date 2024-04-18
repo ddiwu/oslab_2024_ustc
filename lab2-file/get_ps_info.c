8117e3a68b5a15d2a5e5bc7e4e93cf0d000ca388
wudi <wd3271391891@mail.ustc.edu.cn>
2024-04-18 21:46:51 +0800

    fault

--- lab2-file/get_ps_info.c
+++ lab2-file/get_ps_info.c
@@ -7,16 +7,16 @@
 int main(void)
 {
     int num1, num2;
-    int pid1[400], pid2[400],pid[400];
-    char comm1[400][16], comm2[400][16],comm[400][16];
-    int sum_exec_runtime1[400], sum_exec_runtime2[400], sum_exec_runtime[400];
-    int isrunning1[400], isrunning2[400], isrunning[400];
-    double cpu1[400],cpu2[400], cpu[400];
+    int pid1[50], pid2[50],pid[50];
+    char comm1[50*16], comm2[50*16],comm[50*16];
+    int sum_exec_runtime1[50], sum_exec_runtime2[50], sum_exec_runtime[50];
+    int isrunning1[50], isrunning2[50], isrunning[50];
+    double cpu1[50],cpu2[50], cpu[50];
     int cnt;
 
     int t_p;
-    char t_comm[16];
-    int t_runtime;
+    char t_comm;
+    unsigned long long t_runtime;
     int t_isrunning;
     double t_cpu;
     int temp = 0;
@@ -25,24 +25,28 @@ int main(void)
     syscall(322, &num1, pid1, comm1, isrunning, sum_exec_runtime1);
     while(1)
     {
+
+        printf("num = %d",num1);
         sleep(1);
         syscall(322, &num2, pid2, comm2, isrunning, sum_exec_runtime2);
         cnt = 0;
-        for(int i = 0; i< num2; i++)
-        {
-            for(int j = 0; j < num1; j++)
-            {
-                if(strcmp(comm1[j], comm2[i]) == 0)
+        int i = 0, j = 0;
+        while (i < num1 && j < num2) {
+            if (pid2[i] == pid1[j]) {
+                pid[cnt] = pid2[i];
+                for(int k = 0; k< 16; k++)
                 {
-                    pid[i] = pid2[i];
-                    strcpy(comm[i], comm2[i]);
-                    isrunning[i] = isrunning2[i];
-                    sum_exec_runtime[i] = sum_exec_runtime2[i];
-                    cpu[i] = (double)(sum_exec_runtime2[i] - sum_exec_runtime1[j]) / 10000000;//百分比
-                    cnt++;
-                    break;
+                    comm[16*cnt+k] = comm2[16*i+k];
                 }
-            }
+                isrunning[cnt] = isrunning2[i];
+                cpu[cnt] = (sum_exec_runtime2[i] - sum_exec_runtime1[i]) / (double)10000000;//百分比
+                cnt++;
+                i++;
+                j++;
+            } else if (pid1[i] < pid2[j])
+                i++;
+            else if (pid1[i] > pid2[j])
+                j++;
         }
         
         //将这些组数据按照cpu值从大到小排序
@@ -67,22 +71,25 @@ int main(void)
             t_cpu = cpu[i];
             cpu[i] = cpu[temp];
             cpu[temp] = t_cpu;
-            strcpy(t_comm, comm[i]);
-            strcpy(comm[i], comm[temp]);
-            strcpy(comm[temp], t_comm);
+            for(int k = 0; k<16;k++)
+            {
+                t_comm = comm[16*i+k];
+                comm[16*i+k] = comm[16*temp+k];
+                comm[16*temp+k] = t_comm;
+            }
         }
         
         system("clear");
         printf("%-5s%-16s%-10s%-5s%-8s\n", "PID", "COMM", "ISRUNNING", "CPU","TIME");
         for (int i = 0; i < 20; i++) {
-            printf("%-5d%-16s%-10d%-2.3lf%%%-2.3lf\n", pid[i], comm[i], isrunning[i], cpu[i], (double)sum_exec_runtime[i]/1000000000);
+            printf("%-5d %c %-10d %-2.3lf%% %-2.3lf\n", pid[i], comm[16*i], isrunning[i], cpu[i], sum_exec_runtime[i]/(double)1000000000);
         }
 
         num1 = num2;
-        memcpy(pid1, pid2, sizeof(int) * 400);
-        memcpy(comm1, comm2, sizeof(char) * 400 * 16);
-        memcpy(isrunning1, isrunning2, sizeof(int) * 400);
-        memcpy(sum_exec_runtime1, sum_exec_runtime2, sizeof(unsigned long long/*u64*/) * 400);
+        memcpy(pid1, pid2, sizeof(int) * 50);
+        memcpy(comm1, comm2, sizeof(char) * 50 * 16);
+        memcpy(isrunning1, isrunning2, sizeof(int) * 50);
+        memcpy(sum_exec_runtime1, sum_exec_runtime2, sizeof(unsigned long long/*u64*/) * 50);
 
     }
 }
